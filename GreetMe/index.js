87a1c99a2c90950a23bc1756bf8afe848be53cec
subhadeep majumder <subhadeep.majumder@aveva.com>
2019-09-23 19:22:39 +0530

    GreetMe lambda Function

---
+++ GreetMe/index.js
@@ -0,0 +1,31 @@
+const moment = require('moment');
+
+var greetings = {
+    'en' : 'Hello',
+    'fr' : 'Bonjour',
+    'hi' : 'Namaste',
+    'es' : 'Hola',
+    'pt' : 'ola',
+    'it' : 'Ciao',
+    'de' : 'Hallo'
+};
+
+exports.handler = async (event) => {
+    let name = event.pathParameters.name;
+    let {lang, ...info} = event.queryStringParameters;
+
+    let message = `${greetings[lang] ? greetings[lang] : greetings['en']} ${name}`;
+
+    let response = {
+        message: message,
+        info: info,
+        timestamp: moment.unix()
+    };
+    return {
+        statusCode: 200,
+        headers :{
+            "Access-Control-Allow-Origin": "*"
+        },
+        body: JSON.stringify(response)
+    };
+};
